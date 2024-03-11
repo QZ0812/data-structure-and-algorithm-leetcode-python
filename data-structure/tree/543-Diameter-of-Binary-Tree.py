fbe0d3208044764bfb74bf2538eafe683ffeff7c
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-10 22:17:08 -0400

    Create 543-Diameter-of-Binary-Tree.py

---
+++ data-structure/tree/543-Diameter-of-Binary-Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        max_diameter = 0
+        
+        def max_Depth(root):
+            nonlocal max_diameter
+            
+            if not root:
+                return 0
+
+            l = max_Depth(root.left)
+            r = max_Depth(root.right)
+
+            max_diameter = max(l + r, max_diameter)
+
+            return 1 + max(l , r)
+      
+        max_Depth(root)
+        return max_diameter
