09d349ed6ebf404a25ef1b9870f1005ed312e0c2
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-10 23:16:28 -0400

    Create 437-Path-Sum-3.py

---
+++ data-structure/tree/437-Path-Sum-3.py
@@ -0,0 +1,35 @@
+# high voted anwser in leetcode: https://leetcode.com/problems/path-sum-iii/discuss/141424/Python-step-by-step-walk-through.-Easy-to-understand.-Two-solutions-comparison.-%3A-)
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        if not root:
+            return 0
+        
+        ans = self.find_path_sum_with_root(root, targetSum, 0)
+        ans += self.pathSum(root.left, targetSum) 
+        ans += self.pathSum(root.right, targetSum) 
+                
+        return ans
+    
+                
+    def find_path_sum_with_root(self, root, targetSum, count):
+
+        if not root:
+            return count
+
+        if targetSum == root.val:
+            count += 1
+
+        count = self.find_path_sum_with_root(root.left, targetSum - root.val, count)
+        count = self.find_path_sum_with_root(root.right, targetSum - root.val, count)
+
+        return count
