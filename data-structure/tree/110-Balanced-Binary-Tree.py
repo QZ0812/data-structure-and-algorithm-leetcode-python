fe99a1d0483094c1d684e1989d9d6cb9dddad837
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-10 20:10:36 -0400

    Create 110-Balanced-Binary-Tree.py

---
+++ data-structure/tree/110-Balanced-Binary-Tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+#similar as leetcode 104, but addition step is to check left max and right max height difference. 
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        return not self.maxDepth(root) == -1
+        
+    
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+        
+        if (abs(l - r) > 1 or l == -1 or r == -1):
+            return -1
+        
+        return 1 + max(l, r)
