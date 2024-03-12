64addfc6e66895a85361a34ef64f2e18b0676556
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-11 22:42:34 -0400

    Update 206-Reverse-Linked-List.py

--- data-structure/linked-list/206-Reverse-Linked-List.py
+++ data-structure/linked-list/206-Reverse-Linked-List.py
@@ -1,1 +1,41 @@
-#
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+# recursion 
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def dfs(head, pre):
+            if not head:
+                return pre
+            post = head.next
+            head.next = pre
+            
+            return dfs(post, head)
+            
+        return dfs(head, None)
+
+
+
+# iterative 
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pre, cur = None, head
+        while cur:
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+            
+        return pre
+
+
+
