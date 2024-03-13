70219b79da67618ac5e8d3b892c0550ad74627a7
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-12 23:52:00 -0400

    Update 234-Palindrome-Linked-List.py

--- data-structure/linked-list/234-Palindrome-Linked-List.py
+++ data-structure/linked-list/234-Palindrome-Linked-List.py
@@ -29,3 +29,21 @@ class Solution:
             head = head.next
         
         return True
+
+
+# O(n) time and O(n) space
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        nums = []
+        while head:
+            nums.append(head.val)
+            head = head.next
+            
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            if nums[l] != nums[r]:
+                return False
+            l += 1
+            r -= 1
+            
+        return True
