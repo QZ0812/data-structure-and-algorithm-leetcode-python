c10a2fc273b419e7c9c3020fecbd235a46bca96b
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-15 00:36:36 -0400

    Create 205-Isomorphic-Strings.py

---
+++ data-structure/string/205-Isomorphic-Strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(set(s))!= len(set(t)):
+            return False
+        
+        rep, ans= {}, ""
+
+        for i in range(len(s)):
+            if s[i] not in rep:
+                rep[s[i]] = t[i]
+                ans += t[i]
+            else:
+                ans += rep[s[i]]
+        
+        return True if ans == t else False
