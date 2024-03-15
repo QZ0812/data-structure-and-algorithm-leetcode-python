2ff11bf2c806b230b6749d205502c52b3dc78929
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-15 00:39:53 -0400

    Update 205-Isomorphic-Strings.py

--- data-structure/string/205-Isomorphic-Strings.py
+++ data-structure/string/205-Isomorphic-Strings.py
@@ -2,11 +2,12 @@
 # Space Complexity: O(n)
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        # make sure it is one to one mapping
         if len(set(s))!= len(set(t)):
             return False
         
         rep, ans= {}, ""
-
+        # make sure index match
         for i in range(len(s)):
             if s[i] not in rep:
                 rep[s[i]] = t[i]
