45acc1f76ab6b595fa1e219e02fc1c6996bc1da4
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-16 17:26:40 -0400

    Create 2-Longest-Substring-Without-Repeaating-Characters.py

---
+++ data-structure/string/2-Longest-Substring-Without-Repeaating-Characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l, ans, count = 0, 0, set()
+        
+        for r in range(len(s)):
+            while s[r] in count:
+                count.remove(s[l])
+                l += 1
+                    
+            count.add(s[r])
+            ans = max(ans, r - l + 1)
+
+        return ans
