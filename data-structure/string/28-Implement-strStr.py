bb152c380c94107bd8d643e6ff95811e7dc65f83
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-16 13:34:55 -0400

    Create 28-Implement-strStr.py

---
+++ data-structure/string/28-Implement-strStr.py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        i, ans =0, -1
+        lenh, lenn = len(haystack), len(needle)
+
+        while i < lenh - lenn + 1:
+            if haystack[i] == needle[0] and haystack[i: i+lenn] == needle:
+                ans = i
+                break
+            i += 1
+
+        return ans
