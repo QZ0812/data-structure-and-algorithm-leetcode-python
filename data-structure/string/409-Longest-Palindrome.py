9ffb5ae6c5a54b83fc7d9aff94bdb44eae6a177e
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-16 15:51:23 -0400

    Update 409-Longest-Palindrome.py

--- data-structure/string/409-Longest-Palindrome.py
+++ data-structure/string/409-Longest-Palindrome.py
@@ -1,10 +1,12 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         count, ans = {}, 0
         
         for item in s:
             count[item] = 1 if item not in count else count[item] + 1
-            
+
         for v in count.values():
             if v % 2 != 0:
                 ans += 1
