7c8e668fd600a872a4a31f0cc007f153249851b8
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-25 12:20:26 -0400

    Update 128-Longest-Consecutive-Sequence.py

--- data-structure/hashmap/128-Longest-Consecutive-Sequence.py
+++ data-structure/hashmap/128-Longest-Consecutive-Sequence.py
@@ -18,3 +18,19 @@ class Solution:
             ans = max(ans, nex - pre - 1)
             
         return ans
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:  
+        nums, result = set(nums), 0
+        
+        for num in nums:
+            current_seq = 1
+            if num-1 not in nums:
+                while num+1 in nums:
+                    current_seq+=1
+                    num+=1
+                    
+            result = max(result, current_seq)
+            
+        return result
