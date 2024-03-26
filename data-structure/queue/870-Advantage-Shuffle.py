7d68c81cd60aa9f2545e6064533ede1f16cae06b
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-26 17:35:43 -0400

    Create 870-Advantage-Shuffle.py

---
+++ data-structure/queue/870-Advantage-Shuffle.py
@@ -0,0 +1,21 @@
+# time O(n*logn)
+# space 0(n)
+import heapq
+class Solution:
+    def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        ans = [0  for _ in range(len(nums1))]
+        hp = []
+        for i, num2 in enumerate(nums2):
+            heapq.heappush(hp, (-num2, i))
+
+        while nums1:
+            num2, i = heapq.heappop(hp)
+            if nums1[-1] > abs(num2):
+                ans[i] = nums1[-1]
+                nums1.pop()
+            else:
+                ans[i] = nums1[0]
+                nums1.remove(nums1[0])
+                
+        return ans
