aa4431de1bd92c9abdc4adc7b77a7416f8309935
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-26 12:40:21 -0400

    Create 594-Longest-Harmonious-Subsequence.py

---
+++ data-structure/hashmap/594-Longest-Harmonious-Subsequence.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        count, ans = {}, 0
+        
+        for num in nums:
+            count[num] = 1 if num not in count else count[num] + 1
+            
+        while count:
+            curr, freq = count.popitem()
+            pre, nex = curr - 1, curr + 1
+            
+            currfreq = freq
+            while pre in count:
+                ans = max(ans, currfreq + count[pre])
+                currfreq = count.pop(pre)
+                pre = pre - 1
+            
+            prefreq = freq 
+            while nex in count:
+                ans = max(ans, prefreq + count[nex])
+                prefreq = count.pop(nex)
+                nex = nex + 1
+                
+        return ans
