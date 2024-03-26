744cef4372f8f8a50486ab59ec9332dcf1a68043
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-25 23:25:30 -0400

    Create 503-Next-Greater-Element-II.py

---
+++ data-structure/stack/503-Next-Greater-Element-II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        lenth, stack = len(nums), []
+        ans = [-1]*lenth
+
+        nums = nums + nums
+        for i, num in enumerate(nums):
+            while stack and num > stack[-1][1]:
+                index, val = stack.pop()
+                if index < lenth:
+                    ans[index] = num
+            stack.append((i, num))
+        return ans
