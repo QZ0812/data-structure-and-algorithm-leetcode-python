1a3e762fa72977b308bb8cf5c29aa441f183fa6a
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-03-26 12:45:54 -0400

    Update 594-Longest-Harmonious-Subsequence.py

--- data-structure/hashmap/594-Longest-Harmonious-Subsequence.py
+++ data-structure/hashmap/594-Longest-Harmonious-Subsequence.py
@@ -23,3 +23,17 @@ class Solution:
                 nex = nex + 1
                 
         return ans
+
+# time/space O(n)
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:   
+        count, ans = {}, 0
+        
+        for num in nums:   
+            count[num] = 1 if num not in count else count[num] + 1
+
+        for key, value in count.items():
+            if key+1 in count and count[key+1]+value > ans:
+                ans = count[key+1]+value
+                
+        return ans
