cf5b35beec3d362e051168b5bfe0c6a22d0afab4
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-04-06 01:03:35 -0400

    Update 210-Course-Schedule-II.py

--- data-structure/graph/210-Course-Schedule-II.py
+++ data-structure/graph/210-Course-Schedule-II.py
@@ -32,3 +32,30 @@ class Solution:
         while stack:
             ans.append(stack.pop())
         return ans
+
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for i in range(numCourses)]
+        visit = [0] * numCourses
+        for i in range(len(prerequisites)):
+            graph[prerequisites[i][1]].append(prerequisites[i][0])
+            visit[prerequisites[i][0]] += 1
+
+        stack, ans = [], []
+        for i in range(numCourses):
+            if not visit[i]:
+                stack.append(i)
+                
+        while stack:
+            cur = stack.pop(0)
+            ans.append(cur)
+            for i in graph[cur]:
+                visit[i] -= 1
+                if not visit[i]:
+                    stack.append(i)
+                    
+        for i in range(len(visit)):
+            if visit[i]:
+                return []
+        return ans
