b27db18e60be344564015f4271fcf7f260d7ed04
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-04-06 22:56:27 -0400

    Update 207-Course-Schedule.py

--- data-structure/graph/207-Course-Schedule.py
+++ data-structure/graph/207-Course-Schedule.py
@@ -4,6 +4,36 @@ class Solution:
             return True
         
         graph = [[] for _ in range(numCourses)]
+        for i in range(len(prerequisites)):
+            graph[prerequisites[i][0]].append(prerequisites[i][1])
+            
+        cycle, visit = set(), set()
+        def dfs(item):
+            if item in cycle:
+                return False
+            if item in visit:
+                return True
+            cycle.add(item)
+            for itm in graph[item]:
+                if not dfs(itm):
+                    return False
+            cycle.remove(item)
+            visit.add(item)
+            return True
+                
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return False
+            
+        return True
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
+            return True
+        
+        graph = [[] for _ in range(numCourses)]
         flag = [0] * numCourses
         for i in range(len(prerequisites)):
             graph[prerequisites[i][0]].append(prerequisites[i][1])
