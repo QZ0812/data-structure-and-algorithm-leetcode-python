ffeed93870ee6c03fc5a7dd3d1b17828809beecf
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-04-11 21:47:36 -0400

    Update 200-Number-of-Islands.py

--- data-structure/graph/200-Number-of-Islands.py
+++ data-structure/graph/200-Number-of-Islands.py
@@ -3,6 +3,7 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
+        
         row, col, count = len(grid), len(grid[0]), 0
         visit = [[False] * col for _ in range(row)]
         
@@ -13,21 +14,17 @@ class Solution:
                 return
             
             visit[i][j] = True
+            
+            bfs(i-1, j)
+            bfs(i+1, j)
+            bfs(i, j-1)
+            bfs(i, j+1)
 
-            if i - 1 >= 0 and grid[i-1][j] == "1":
-                bfs(i-1, j)
-            if i + 1 < row and grid[i+1][j] == "1":
-                bfs(i+1, j)
-            if j - 1 >= 0 and grid[i][j-1] == "1":
-                bfs(i, j-1)
-            if j + 1 < col and grid[i][j+1] == "1":
-                bfs(i, j+1)
-        
         for i in range(row):
             for j in range(col):
                 if not visit[i][j]:
-                    bfs(i, j)
                     if grid[i][j] == "1":
+                        bfs(i, j)
                         count += 1
                     
         return count
