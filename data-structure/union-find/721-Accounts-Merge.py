6029185b09d608bf3e357354cf82c04e896bddea
QZ0812 <50255817+QZ0812@users.noreply.github.com>
2024-04-14 10:12:58 -0400

    Create 721-Accounts-Merge.py

---
+++ data-structure/union-find/721-Accounts-Merge.py
@@ -0,0 +1,32 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parents = [i for i in range(n)]
+    
+    def find(self, i):
+        if i != self.parents[i]:
+            self.parents[i] = self.find(self.parents[i])
+        return self.parents[i]
+    
+    def union(self, i, j):
+        irep = self.find(i)
+        jrep = self.find(j)
+        self.parents[irep] = jrep
+        
+    
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind(len(accounts))
+        
+        ownership = {}
+        for i, (_, *emails) in enumerate(accounts):
+            for email in emails:
+                if email in ownership:
+                    uf.union(i, ownership[email])
+                ownership[email] = i
+        
+        # Append emails to correct index
+        ans = collections.defaultdict(list)
+        for email, owner in ownership.items():
+            ans[uf.find(owner)].append(email)
+        
+        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
